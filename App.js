efc1bfc7715b4b7909fe7cd4780beadd5aa7eded
Winter Hardcastle <winter@hardcastle.io>
2023-04-11 10:41:51 -0400

    Create project with Expo and basic counter functionality

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Button, Text, View } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>This is react native!</Text>
+      <Text style={styles.counter}>{counter}</Text>
+      <Button
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+        title="Increment"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  counter: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#23ff43",
   },
 });
